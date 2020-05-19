bc7b3f1775276529acb963bdf6e64b093e989215
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-05-18 23:26:03 -0300

    Moving User Service import from app.module.ts to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserService } from './shared/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
   
